8756e08967b628329fc49fe762852a45f910d867
IrionXenix <irion29@gmail.com>
2018-11-21 13:54:36 +0000

    kill me

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<style>
+div
+{
+  height:100px;
+  width:100px;
+  background-color:red;
+}
+</style>
+</head>
+<body>
+<div aria-label="January">
+</div>
+<br>
+<div aria-label="February">
+</div>
+<br>
+<div aria-label="March">
+</div>
+
+</body>
+</html>
+
+
